a3a666513eb770dfce41524877cbfbee4336d52c
Palmanza <paulalmanza@gmail.com>
2022-07-28 19:13:00 -0300

    added hero banner

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/hero_banner.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        CustomScrollView(
+          slivers: [
+            HeroBanner(),
+          ],
+        ),
+      ],
+    ));
+  }
+}
